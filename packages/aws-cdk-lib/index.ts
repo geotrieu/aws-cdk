0a2f361444f59c7c22a301652933f501df5fadbf
Adonis Carvajal <caradoni@amazon.com>
2026-04-08 08:32:51 +0000

    feat(aws-s3files): S3Files Lambda L1 integration

--- packages/aws-cdk-lib/index.ts
+++ packages/aws-cdk-lib/index.ts
@@ -255,6 +255,7 @@ export * as aws_s3_assets from './aws-s3-assets';
 export * as aws_s3_deployment from './aws-s3-deployment';
 export * as aws_s3_notifications from './aws-s3-notifications';
 export * as aws_s3express from './aws-s3express';
+export * as aws_s3files from './aws-s3files';
 export * as aws_s3objectlambda from './aws-s3objectlambda';
 export * as aws_s3outposts from './aws-s3outposts';
 export * as aws_s3tables from './aws-s3tables';
